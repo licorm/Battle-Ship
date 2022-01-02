8687536dcca2ca757ee52769c270aab74349ec69
Lisa Cormier <lcormier5@hotmail.com>
2022-01-02 15:50:38 -0700

    remove unneccessary console logs

--- src/components/battleship.js
+++ src/components/battleship.js
@@ -1,10 +1,6 @@
 
 export default function BattleShip(props) {
-  console.log(props)
-  // const battleEach = props.battleShips.map((battleship) => {
-  //   console.log(battleship)
-  //   return <button id={battleship} className='BattleShip' onClick={props.onSelect(battleship)}>{battleship}</button>
-  // })
+  
   return (
     <div>
       {props.battleShips.includes('Carrier') && <button id='Carrier' className='BattleShip' onClick={() => props.onSelect('Carrier')}>Carrier</button> }
